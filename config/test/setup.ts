3760c20e48f8736b5c3cd3bcfe2eade9a5fdbea3
elbadry <miliarder96@gmail.com>
2023-11-11 14:38:56 +0200

    add CI file
    install deb libs
    
    fix typo
    
    fix port
    
    run start bash script
    
    test 2

--- config/test/setup.ts
+++ config/test/setup.ts
@@ -12,6 +12,7 @@ declare global {
 beforeAll(async () => {
   process.env.JWT_KEY = 'my-secret-key';
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  process.env.NODE_ENV = 'test';
 
   await db.migrate.rollback();
   await db.migrate.latest();
